c4782744c572280c74f72d5b8037c9ae9d9061d7
Kevin Walter <kevin.walter.private@googlemail.com>
2015-01-09 03:59:19 +0100

    add more missing files

--- src/main/scala/chrome/runtime/bindings/MessageSender.scala
+++ src/main/scala/chrome/runtime/bindings/MessageSender.scala
@@ -1,10 +1,12 @@
 package chrome.runtime.bindings
 
+import chrome.tabs.bindings.Tab
+
 import scala.scalajs.js
 
 class MessageSender extends js.Object {
 
-  def tab: js.UndefOr[chrome.tabs.Tab] = js.native
+  def tab: js.UndefOr[Tab] = js.native
   def frameId: js.UndefOr[Int] = js.native
   def id: js.UndefOr[String] = js.native
   def url: js.UndefOr[String] = js.native
---
+++ src/main/scala/chrome/tabs/bindings/Tab.scala
@@ -0,0 +1,40 @@
+package chrome.tabs.bindings
+
+import scala.scalajs.js
+
+class Tab extends js.Object {
+
+  def id: js.UndefOr[Int] = js.native
+  def index: Int = js.native
+  def windowId: Int = js.native
+  def openerTabId: js.UndefOr[Int] = js.native
+
+  def highlighted: Boolean = js.native
+  def active: Boolean = js.native
+  def pinned: Boolean = js.native
+  
+  def url: js.UndefOr[String] = js.native
+  def title: js.UndefOr[String] = js.native
+  def favIconUrl: js.UndefOr[String] = js.native
+  def status: js.UndefOr[Tab.Status] = js.native
+  
+  def incognito: Boolean = js.native
+  
+  def width: js.UndefOr[Int] = js.native
+  def height: js.UndefOr[Int] = js.native
+  def sessionId: js.UndefOr[String] = js.native
+
+}
+
+object Tab {
+  
+  type Status = String
+  
+  object StatusValues {
+
+    val LOADING: Status = "loading"
+    val COMPLETE: Status = "complete"
+
+  }
+  
+}
