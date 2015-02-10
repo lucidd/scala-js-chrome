b58fee146e1c39668a031655a047b5cf046c1535
Kevin Walter <kevin.walter.private@googlemail.com>
2015-02-10 16:52:32 +0100

    Fix refactoring error

--- src/main/scala/chrome/system/network/Network.scala
+++ src/main/scala/chrome/system/network/Network.scala
@@ -12,7 +12,7 @@ object Network {
     bindings.Network.getNetworkInterfaces((interfaces: js.Array[bindings.NetworkInterface]) => {
       promise.complete(chrome.lastErrorOrValue {
         (for ((name, iface) <- interfaces.groupBy(_.name)) yield {
-          val configs = iface.map(x => new NetworkConfig(x.address, x.prefixLength))
+          val configs = iface.map(x => new Interface.Config(x.address, x.prefixLength))
           new Interface(name, configs.toList)
         }).toList
       })
