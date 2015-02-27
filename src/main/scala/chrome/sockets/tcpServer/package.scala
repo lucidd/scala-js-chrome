b86e273fd6bef4d8fc9b32c6fbea814968d524d8
Kevin Walter <kevin.walter.private@googlemail.com>
2015-02-27 02:29:43 +0100

    add chrome.sockets.tcpServer.bindings.SocketId extentions

---
+++ src/main/scala/chrome/sockets/tcpServer/package.scala
@@ -0,0 +1,39 @@
+package chrome.sockets
+
+import chrome.sockets.tcpServer.bindings.{SocketInfo, SocketProperties, CreateInfo, SocketId}
+
+import scala.concurrent.{Promise, Future}
+import scala.scalajs.js
+
+
+package object tcpServer {
+
+  implicit class Socket(val socketId: SocketId) extends AnyVal {
+
+    def update(properties: SocketProperties): Future[Unit] = {
+      TCPServer.update(socketId, properties)
+    }
+
+    def setPaused(paused: Boolean): Future[Unit] = {
+      TCPServer.setPaused(socketId, paused)
+    }
+
+    def listen(address: String, port: Int, backlog: js.UndefOr[Int] = js.undefined): Future[Int] = {
+      TCPServer.listen(socketId, address, port, backlog)
+    }
+
+    def disconnect: Future[Unit] = {
+      TCPServer.disconnect(socketId)
+    }
+
+    def close: Future[Unit] = {
+      TCPServer.close(socketId)
+    }
+
+    def getInfo: Future[SocketInfo] = {
+      TCPServer.getInfo(socketId)
+    }
+
+  }
+
+}
