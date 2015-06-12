e9d6c36cc6bf7ef60189b204e15ff54400b41435
Kevin Walter <kevin.walter.private@googlemail.com>
2015-06-12 22:55:50 +0200

    add ChromeApp entrypoint helper

---
+++ bindings/src/main/scala/utils/ChromeApp.scala
@@ -0,0 +1,23 @@
+package utils
+
+import chrome.app.runtime.Runtime
+import chrome.app.runtime.bindings.{LaunchData, Request}
+
+import scala.scalajs.js
+
+
+trait ChromeApp extends js.JSApp {
+
+  def main(): Unit = {
+    Runtime.onLaunched.listen(onLaunched)
+    Runtime.onRestarted.listen((_) => onRestart)
+    Runtime.onEmbedRequested.listen(onEmbedRequested)
+  }
+
+  def onLaunched(launchData: LaunchData): Unit = {}
+
+  def onRestart(): Unit = {}
+
+  def onEmbedRequested(request: Request): Unit = {}
+
+}
