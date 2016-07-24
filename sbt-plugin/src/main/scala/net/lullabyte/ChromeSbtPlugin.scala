f5fadbe2722bd6036c539659cc01c27046b3e453
Kevin Walter <kevin.walter.private@googlemail.com>
2016-07-24 11:34:50 +0200

    package minifiedDependencies for chromeUnpackedOpt and add chromeUnpackedFast #fixes #12

--- sbt-plugin/src/main/scala/net/lullabyte/ChromeSbtPlugin.scala
+++ sbt-plugin/src/main/scala/net/lullabyte/ChromeSbtPlugin.scala
@@ -13,26 +13,35 @@ object ChromeSbtPlugin extends AutoPlugin {
 
   object autoImport {
 
-    val chromeBuildOpt = TaskKey[File]("chromeBuildOpt")
-    val chromeBuildFast = TaskKey[File]("chromeBuildFast")
+    val chromeUnpackedOpt = TaskKey[File]("chromeUnpackedOpt")
+    val chromeUnpackedFast = TaskKey[File]("chromeUnpackedFast")
     val chromePackage = TaskKey[File]("chromePackage")
     val chromeGenerateManifest = TaskKey[File]("chromeGenerateManifest")
     val chromeManifest = TaskKey[Manifest]("chromeManifest")
 
 
     lazy val baseSettings: Seq[Def.Setting[_]] = Seq(
-      chromeBuildOpt := {
-        Chrome.buildExtentionDirectory(target.value / "chrome" / "unpacked")(
+      chromeUnpackedFast := {
+        Chrome.buildUnpackedDirectory(target.value / "chrome" / "unpacked-fast")(
+          (chromeGenerateManifest in Compile).value,
+          (fastOptJS in Compile).value.data,
+          (packageJSDependencies in Compile).value,
+          (packageScalaJSLauncher in Compile).value.data,
+          (resourceDirectories in Compile).value
+        )
+      },
+      chromeUnpackedOpt := {
+        Chrome.buildUnpackedDirectory(target.value / "chrome" / "unpacked-opt")(
           (chromeGenerateManifest in Compile).value,
           (fullOptJS in Compile).value.data,
-          (packageJSDependencies in Compile).value,
+          (packageMinifiedJSDependencies in Compile).value,
           (packageScalaJSLauncher in Compile).value.data,
           (resourceDirectories in Compile).value
         )
       },
       chromePackage := {
         val out = target.value / "chrome"
-        val chromeAppDir = chromeBuildOpt.value
+        val chromeAppDir = chromeUnpackedOpt.value
         val zipFile = new File(out, s"${name.value}.zip")
         IO.zip(allSubpaths(chromeAppDir), zipFile)
         zipFile
